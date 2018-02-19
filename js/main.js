939ccecca3e3273b6513e0e7fe0ad8273dac6191
Araya <Guiselle.Araya@disney.com>
2018-02-19 14:49:23 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,17 +3,24 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + cardOne);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + cardTwo);
-//varified this works up to here
-
+var checkForMatch = function() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+  } else {
+  console.log("Sorry, try again.");
+  }
+}
+var flipCard = function(cardId){
+  console.log("User flipped" + cards[cardId])
+  cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
 } else {
   alert("Sorry, try again.");
   }
+}
+
+checkForMatch();
+flipCard(0);
+flipCard(2);
